4fea9f527343d932bef67f16d8e53c85a587cb96
b0x3n <b0x3n4dm1n@gmail.com>
2024-09-20 10:29:45 +0100

    Wee fix...

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -10,8 +10,8 @@
 
     const Main = () => {
 
-        window.__url = '';
-        // window.__url = 'https://b0x3n.github.io/narplan';
+        // window.__url = '';
+        window.__url = 'https://b0x3n.github.io/narplan';
 
         const   __app = App();
 
